7923e5117ef269260f3b10d47d517538fbe8b3ee
iharmanpannu <iharmanpannu@gmail.com>
2018-01-16 23:22:33 -0800

    changes

--- final/src/components/About/AboutPage.js
+++ final/src/components/About/AboutPage.js
@@ -9,19 +9,27 @@ const AboutPage = () => (
     <section className="about" color="#16a085">
       <h1>About</h1>
       <div>
-        <p>Hi, I'm Harman Pannu.</p>
+        <p>
+          Hi, I'm <span className="h">H</span>arman Pannu.
+        </p>
         <p>
           Experienced in Building Frontend Web Applications(React JS) and Static
           Websites(Html, CSS, JS) and seeking to Learn and Grow under Mentorship
           of Proficient Developers. Passionate about learning new technologies
-          and solving problems. " There are no secrets to success. It is the
-          result of preparation, hard work, and learning from failure. " I code
-          every day and when I am not coding I read books, watch programming
-          tutorials online, listen to podcasts, learn new technologies and
-          workout. Check out my project on -{" "}
+          and solving problems.{" "}
+        </p>
+        <p>
+          I code every day and when I am not coding I read books, watch
+          programming tutorials online, listen to podcasts, learn new
+          technologies and workout. Check out my project on -
           <a href="https://github.com/iharmanpannu">Github </a>
           I look forward to applying my coding experience to a career in tech.
         </p>
+        <br />
+        <i>
+          " There are no secrets to success. It is the result of preparation,
+          hard work, and learning from failure. "
+        </i>
       </div>
 
       <div>
@@ -47,24 +55,42 @@ const AboutPage = () => (
               Karnataka State Open University - Bachelors of Computer
               Applications 2016
             </li>
+            <br />
             <li>
               Codeacademy - Nov 2017 - Jan 2018- Build front-end Web
               Applications from scratch -
             </li>
+            <ul className="list">
+              <h4>Technologies I Learned</h4>
+              <li>JavaScript</li>
+              <li>React JS</li>
+              <li>Web API's</li>
+              <li>AJAX, XHR and JSON</li>
+            </ul>
+            <br />
             <li>
               TeamTreeHouse - Jan 2018 - Present - Tech Degree - Full Stack
               JavaScript
             </li>
-              <h4>Technologies I am currently Learning at Treehouse </h4>
-              <ul className="list">
-                <li>JavaScript</li>
-                <li>jQuery</li>
-                <li>Node.js</li>
-                <li>Angular</li>
-                <li>Express</li>
-                <li>MongoDB</li>
-              </ul>
+            <ul className="list">
+              <h4>Technologies I am currently Learning at Treehouse</h4>
+              <li>JavaScript</li>
+              <li>React JS</li>
+              <li>jQuery</li>
+              <li>Node.js</li>
+              <li>Express</li>
+              <li>MongoDB</li>
+            </ul>
+            <br />
+            <li>
+              TeamTreeHouse - Aug 2017 - Present - Frontend Web Development
             </li>
+            <ul className="list">
+              <h4>Technologies I Learned</h4>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>SCSS</li>
+            </ul>
           </ul>
         </div>
       </div>
